00e74c61504b9d4af6263b25e2250e88522014d9
Nathalie Neuhaus <nneuhaus@ucvts.org>
2019-11-26 13:19:26 -0500

    i really tried

---
+++ test-question.js
@@ -0,0 +1,24 @@
+const readlineSync = require ("readline-sync")
+let
+const convertCtoF = ("c"*(9/5)) + 32
+const convertCtoK = c + 273.15
+const convertFtoC = (f-32) * (5/9)
+const convertFtpK = (f-32) * (5/9) + 273.15
+const convertKtoC = k-273.15
+const convertKtoF = (k-273.15) * (9/5) + 32
+let origin = question.readlineSync("Enter an origin scale:")
+
+while (origin == "f" || origin == "F"|| origin == "K"|| origin == "k" || origin == "C" || origin == "c") {
+  destination = question.readlineSync("Enter a destination scale:")
+}
+while (origin != "f" || origin != "F"|| origin != "K"|| origin != "k" || origin != "C" || origin != "c") {
+  origin = question.readlineSync("Enter an origin scale:")
+}
+while (destination == "f" || destination == "F"|| destination == "K"|| destination == "k" || destination == "C" || destination == "c") {
+      temperature = question.readlineSync("Enter a temperature: ")
+while (origin != "f" || origin != "F"|| origin != "K"|| origin != "k" || origin != "C" || origin != "c") {
+    destination = question.readlineSync("Enter a destination scale:")
+    }
+  while (destination != "f" || destination != "F"|| destination != "K"|| destination != "k" || destination != "C" || destination != "c") {
+}
+}
